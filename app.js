7a41b6eccb61cf25693062bfd1fb08eaa1ab87dc
Mariano <marianomuchiut@gmail.com>
2022-05-21 11:16:09 -0300

    se agrega parte del formulario de registro

--- app.js
+++ app.js
@@ -12,9 +12,16 @@ const public = path.join(__dirname, 'public/');
 // HTTP routes
 const httpRaiz = '/';
 const httpHome ='/home';
+const httpLogin ='/login';
+const httpRegister ='/register';
+const httpCart = '/cart';
+
 
 // HTML
 const homeHtml = 'home.html';
+const loginHtml = 'login.html';
+const registerHtml = 'register.html';
+const cartHtml = 'cart.html';
 
 // Define the static file path
 app.use(express.static(__dirname + '/public/'));
@@ -27,6 +34,17 @@ app.get(httpHome, (req,res) =>{
     res.sendFile(path.join(views, homeHtml))
 })
 
+app.get(httpLogin, (req,res) =>{
+    res.sendFile(path.join(views, loginHtml))
+})
+
+app.get(httpRegister, (req,res) =>{
+    res.sendFile(path.join(views, registerHtml))
+})
+
+app.get(httpCart, (req,res) =>{
+    res.sendFile(path.join(views, cartHtml))
+})
 app.listen(port, () =>{
     console.log(`Example app listenig at http://localhost:${port}`)
     console.log(__dirname)
